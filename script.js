f19d29d6f84508282ec6a8793817de55bed50360
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-03-13 09:25:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ let input=document .getElementById("evaluatedText");
 input.addEventListener("input", ()=>{
 	const textLength=document.getElementById("letterCount");
 
-	textLength.innerText=input.value.length;
+	textLength.innerText=parseFloat(input.value.length);
 })
